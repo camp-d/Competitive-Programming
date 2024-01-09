94d84df38dc1a2d5f31dee2bec33c35b8c7e57c1
dave <dmcamp@g.clemson.edu>
2024-01-09 06:10:50 -0500

    not found finished

--- ATCODER/bootcamp/not_found/temp.cpp
+++ ATCODER/bootcamp/not_found/temp.cpp
@@ -4,7 +4,29 @@ typedef long long ll;
 
 void solve(){
 
+    std::string word;
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+    std::cin >> word;
+
+    std::unordered_map<char, int> chars;
+
+    int count = 0;
+
+    for(auto c : word){
+        if(count == 0 || !chars.contains(c) ){
+            chars.insert(std::make_pair(c, count));
+        }
+        count+=1;
+    }
+    
+    for(auto c : alphabet){
+        if(!chars.contains(c)){
+            std::cout << c << '\n';
+            return;
+        }
+    }
+    std::cout << "None" << '\n';
 }
 
 int main()
