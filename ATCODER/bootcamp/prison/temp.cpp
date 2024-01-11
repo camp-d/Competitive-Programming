733e8a3a9bfc6c56c2bb88d080d3cbbe2c5065d2
dave <dmcamp@g.clemson.edu>
2024-01-11 08:20:22 -0500

    prison finished

--- ATCODER/bootcamp/prison/temp.cpp
+++ ATCODER/bootcamp/prison/temp.cpp
@@ -10,14 +10,20 @@ void solve(){
     std::cin >> num_cards >> num_gates;
 
     std::vector<std::pair<int, int>> cards(num_gates);
+    std::pair<int, int> required_range (0,num_cards);
 
     for(auto x : cards){
         std::cin >> x.first >> x.second;
-        if(x.first >= m){
-
-        }
+        if(x.first > required_range.first)
+            required_range.first = x.first;
+        if(x.second < required_range.second)
+            required_range.second = x.second;
     }
 
+    if( required_range.first > required_range.second)
+        std::cout << 0 << '\n';
+    else
+        std::cout << required_range.second - required_range.first + 1 << '\n';
 }
 
 int main()
