2fdef1260a4e5273bfb82b56e996d72958dd3d16
dave <dmcamp@g.clemson.edu>
2024-01-12 12:29:11 -0500

    changed contest script to produce test input for each problem

---
+++ CF/917/a.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+typedef long long ll;
+
+
+void solve(){
+
+    int size;
+    std::cin >> size;
+
+    std::vector<int> nums(size, 0);
+
+    for(auto &x : nums){
+        std::cin >> x;
+    }
+
+
+
+}
+
+int main()
+{
+std::ios_base::sync_with_stdio(false); 
+std::cin.tie(0); 
+std::cout.tie(0);
+
+int n;
+
+std::cin >> n;
+
+while(n--)
+    solve();
+
+
+return 0;
+}
--- CF/contest.sh
+++ CF/contest.sh
@@ -6,4 +6,11 @@ cp temp.cpp c.cpp
 cp temp.cpp d.cpp
 cp temp.cpp e.cpp
 cp temp.cpp f.cpp
+cp 1.txt a.txt
+cp 1.txt b.txt
+cp 1.txt c.txt
+cp 1.txt d.txt
+cp 1.txt e.txt
+cp 1.txt f.txt
 rm temp.cpp
+rm 1.txt
