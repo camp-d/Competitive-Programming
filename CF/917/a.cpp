40c49b0ec351255b285a778d4149da4de4531ffd
dave <dmcamp@g.clemson.edu>
2024-01-12 12:49:28 -0500

    started CF round 917 attempt

--- CF/917/a.cpp
+++ CF/917/a.cpp
@@ -6,16 +6,33 @@ typedef long long ll;
 void solve(){
 
     int size;
+    int negatives = 0, positives = 0;
+    int count = 0;
     std::cin >> size;
 
     std::vector<int> nums(size, 0);
+    std::vector<int> sign(size, 0);
 
     for(auto &x : nums){
         std::cin >> x;
+        if(x > 0){
+            positives++;
+            sign[count] = 1;
+        }
+        else if(x < 0){
+            negatives++;
+            sign[count] = -1;
+        }
+        else{
+            std::cout << 0 << '\n';
+            return;
+        }
+        count++;
     }
 
+    if(negatives % 2 == 0){
 
-
+    }
 }
 
 int main()
