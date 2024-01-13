3c92580f9000b0ccae14b4e60e6139c868b98256
dave <dmcamp@g.clemson.edu>
2024-01-13 13:49:39 -0500

    fixed templates

--- CF/917/a.cpp
+++ CF/917/a.cpp
@@ -7,31 +7,47 @@ void solve(){
 
     int size;
     int negatives = 0, positives = 0;
+    int zeros = 0;
     int count = 0;
+    int abs_min = -1;
+    int abs_min_index = 0;
+
     std::cin >> size;
 
-    std::vector<int> nums(size, 0);
-    std::vector<int> sign(size, 0);
-
+    std::vector<int> nums(size);
+    
     for(auto &x : nums){
         std::cin >> x;
-        if(x > 0){
-            positives++;
-            sign[count] = 1;
-        }
-        else if(x < 0){
+
+        if(x < 0){
             negatives++;
-            sign[count] = -1;
         }
+        else if( x == 0){
+            zeros++;
+        }
+        /*
         else{
             std::cout << 0 << '\n';
             return;
         }
+        */
+
+        if(abs_min == -1){
+            abs_min = std::abs(x);
+            abs_min_index = count;
+        }
+        else if(std::abs(x) < abs_min){
+            abs_min = std::abs(x);
+            abs_min_index = count;
+        }
         count++;
     }
 
-    if(negatives % 2 == 0){
-
+    if(zeros || negatives % 2 == 1){
+        std::cout << 0 << '\n';
+    }
+    else{
+        std::cout << 1 << '\n' << abs_min_index+1 << ' ' << 0 << '\n';
     }
 }
 
@@ -39,7 +55,6 @@ int main()
 {
 std::ios_base::sync_with_stdio(false); 
 std::cin.tie(0); 
-std::cout.tie(0);
 
 int n;
 
@@ -47,7 +62,6 @@ std::cin >> n;
 
 while(n--)
     solve();
-
 
 return 0;
 }
