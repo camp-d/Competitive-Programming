e85af928f30126cca3abaac1beaa377f6900f403
DC <dmcamp@g.clemson.edu>
2024-03-16 18:37:34 -0400

    shell game finished

--- USACO/shell_game/temp.cpp
+++ USACO/shell_game/temp.cpp
@@ -3,7 +3,19 @@
 typedef long long ll;
 
 void solve(){
+    int trials;
+    int guess = 0;
+    int swap1, swap2;
 
+    std::cin >> trials;
+    std::vector<std::pair<int,int>> shells = {{1,0}, {2,0}, {3,0}};
+
+    while(trials--){
+        std::cin >> swap1 >> swap2 >> guess;
+        std::swap(shells[swap1-1], shells[swap2-1]);
+        shells[guess-1].second++;
+    }
+    std::cout << std::max({shells[0].second, shells[1].second, shells[2].second}) << '\n';
 }
 
 int main()
