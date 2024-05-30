f2ea5a862f7f3bc11b214fad896d7211e0d993c9
dave <dmcamp@g.clemson.edu>
2024-05-30 00:12:01 -0400

    solved ocaml ATCODER problem, need to update makefiles and scripts to support solving problems in ocaml fully

--- ATCODER/bootcamp/maximal_value/temp.ml
+++ ATCODER/bootcamp/maximal_value/temp.ml
@@ -1,31 +1,31 @@
-let rec max ls size result = 
-    if (List.length ls = 1 && size <> 1) then
-        result + (List.hd ls)
-    else if (List.length ls = 1 && size = 1) then
-        result+(List.hd ls)+(List.hd ls)
-    else if(List.length ls = size) then
-        max ls (size-1) (result+(List.hd ls)+(List.nth ls (List.length ls)-1))
+let min a b = 
+    if a < b then
+        a
     else
-        match List.hd ls <= List.hd (List.tl ls) with
-        | true -> max (List.tl ls) (size) (result + (List.hd ls))
-        | false -> max (List.tl ls) (size) (result + (List.hd (List.tl ls)))
-;;
+        b;;
 
-let explode s = List.init (String.length s) (String.get s);;
+let rec max ls on result = 
+    if on = 1 then
+        max ls 0 (result + List.hd ls + (List.nth ls ((List.length ls)-1) ))
+    else
+        match ls with
+        | [] -> result
+        | [x] -> result
+        | h :: t -> max t 0 (result + (min h (List.hd t) ) );;
 
 let rec buildls lst out = 
     match lst with
     | [] -> out
     | h::t -> buildls t ((int_of_string h) :: out);;
 
-let n = Scanf.sscanf (read_line()) "%d" (fun a -> a) in
+let _ = Scanf.sscanf (read_line()) "%d" (fun a -> a) in
 
-let b = Scanf.sscanf (read_line()) "%s" (fun a -> a) in
+let b = read_line() in
 
 let a = String.split_on_char ' ' b in
 
 let ls = buildls a [] in
 
-let answer = max ls (List.length ls) 0 in
+let answer = max ls 1 0 in
 
 print_endline (string_of_int answer)
